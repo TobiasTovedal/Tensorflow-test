a6824171548ef7e284f7a23e4fb1d011ac981ded
Tobias Tovedal <tobias.tovedal@gmail.com>
2017-12-10 14:38:49 +0100

    First commit

---
+++ MNIST.py
@@ -0,0 +1,5 @@
+# https://www.tensorflow.org/get_started/mnist/beginners
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
